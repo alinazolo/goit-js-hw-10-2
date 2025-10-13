bdef3c1285b9f6e6436f3244121c61700873f89d
Alina <alinazolotopup99@gmail.com>
2025-10-13 23:22:07 +0200

    update

--- src/main.js
+++ src/main.js
@@ -26,46 +26,50 @@
 
 
 
-const startBtn = document.querySelector(".start-btn");
-const container = document.querySelector(".containers");
-const result = document.querySelector(".result");
+// const startBtn = document.querySelector(".start-btn");
+// const container = document.querySelector(".containers");
+// const result = document.querySelector(".result");
 
-startBtn.addEventListener("click", handleStartGame);
+// startBtn.addEventListener("click", handleStartGame);
 
-function handleStartGame(event) {
-  event.target.disabled = true;
+// function handleStartGame(event) {
+//   event.target.disabled = true;
 
-  const promises = [...container.children].map(() => {
-    return new Promise((resolve, reject) => {
-      const isPromiseFulfilled = Math.random() > 0.5;
+//   const promises = [...container.children].map(() => {
+//     return new Promise((resolve, reject) => {
+//       const isPromiseFulfilled = Math.random() > 0.5;
 
-      if (isPromiseFulfilled) {
-        resolve("🤩");
-      } else {
-        reject("😢");
-      }
-    });
-  });
+//       if (isPromiseFulfilled) {
+//         resolve("🤩");
+//       } else {
+//         reject("😢");
+//       }
+//     });
+//   });
 
-  result.textContent = "";
+//   result.textContent = "";
 
-  Promise.allSettled(promises).then((items) => {
-    console.log(items);
+//   Promise.allSettled(promises).then((items) => {
+//     console.log(items);
 
-    items.forEach((item, i) => {
-      container.children[i].textContent = "";
-      setTimeout(() => {
-        container.children[i].textContent = item.value || item.reason;
-      }, 1000 * (i + 1));
-    });
+//     items.forEach((item, i) => {
+//       container.children[i].textContent = "";
+//       setTimeout(() => {
+//         container.children[i].textContent = item.value || item.reason;
+//       }, 1000 * (i + 1));
+//     });
 
-    const isWinner =
-      items.every(({ status }) => status === "fulfilled") ||
-      items.every(({ status }) => status === "rejected");
+//     const isWinner =
+//       items.every(({ status }) => status === "fulfilled") ||
+//       items.every(({ status }) => status === "rejected");
 
-    setTimeout(() => {
-      event.target.disabled = false;
-      result.textContent = isWinner ? "Winner 🎉" : "Looser 😅";
-    }, container.children.length * 1000);
-  });
-}
+//     setTimeout(() => {
+//       event.target.disabled = false;
+//       result.textContent = isWinner ? "Winner 🎉" : "Looser 😅";
+//     }, container.children.length * 1000);
+//   });
+// }
+// Описаний в документації
+import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
+import "flatpickr/dist/flatpickr.min.css";
